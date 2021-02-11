f23e1fb38c167446ef85716b40aba15d32743dde
Daniel <daniel.granat87@gmail.com>
2021-02-11 23:48:28 +0100

    First working version
    Signed-off-by: Daniel <daniel.granat87@gmail.com>

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 import { route } from './router.js';
 
-route('/', 'home', function() {
-  this.where = 'here';
+let msg = '';
+const LOGIN_LINK = 'https://zwzt-zadanie.netlify.app/api/login';
+const LOGIN_SUCCESS = 'Login success!';
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+route('/', 'home', function () {
+  this.msg = '';
+
+  this.$on('.login-button', 'click', e => {
+    e.preventDefault();
+
+    const username = document.getElementById('username');
+    const password = document.getElementById('pass');
+
+    postData(LOGIN_LINK, {
+      username: username.value,
+      password: password.value,
+    }).then(data => {
+      msg = data.message;
+
+      if (data.message == LOGIN_SUCCESS) {
+        window.location.href = '#/success';
+      } else {
+        this.msg = msg;
+        this.$refresh();
+      }
+    });
+  });
 });
 
-route('/ex1', 'example1', function() {
-  this.title = 'Example 1';
+route('/success', 'success', function () {
+  this.msg = msg;
 });
 
-route('/ex2', 'example2', function() {
+route('/ex2', 'example2', function () {
   this.title = 'Example 2';
   this.counter = 0;
   this.$on('.my-button', 'click', () => {
