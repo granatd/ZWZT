8bdd210644c561989edd3e48f61e6c4f16097b8f
Daniel <daniel.granat87@gmail.com>
2021-02-14 23:03:31 +0100

    Each template in separate html file
    Signed-off-by: Daniel <daniel.granat87@gmail.com>

--- app.js
+++ app.js
@@ -44,13 +44,4 @@ route('/success', 'success', function () {
   this.msg = msg;
 });
 
-route('/ex2', 'example2', function () {
-  this.title = 'Example 2';
-  this.counter = 0;
-  this.$on('.my-button', 'click', () => {
-    this.counter += 1;
-    this.$refresh();
-  });
-});
-
 route('*', '404', function () {});
